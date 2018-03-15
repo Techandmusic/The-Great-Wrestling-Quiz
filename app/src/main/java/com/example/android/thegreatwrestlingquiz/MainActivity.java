3d97c6702dd893d9e6c9964bde17050e10ba2fb4
Adam <datriphopper@gmail.com>
2018-03-14 19:18:17 -0500

    Added IDs to question and answer sections of activity_main.xml. Changed padding amount to @dimen/small. Added correctAnswers variable, increment method, and checkAnswers method to MainActitvity.java.

--- app/src/main/java/com/example/android/thegreatwrestlingquiz/MainActivity.java
+++ app/src/main/java/com/example/android/thegreatwrestlingquiz/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.android.thegreatwrestlingquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    int correctAnswers = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void increment(View view) {
+        correctAnswers += 1;
+    }
+
+
+
+    public void checkAnswers(View view) {
+        //function body to be added
+
     }
 }
